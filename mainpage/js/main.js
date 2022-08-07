11293d10b96c54fc6cbd8f95b0dfaf174b55880d
BogusD <qazxwd44@hanmail.net>
2022-08-07 13:36:05 +0900

    업데이트 메인페이지

--- mainpage/js/main.js
+++ mainpage/js/main.js
@@ -3,7 +3,6 @@ const body = document.querySelector("body");
 const slideEl = document.querySelectorAll(".slide")
 const slideElR = document.querySelectorAll(".slide_R")
 const slideP=document.querySelector(".slogan")
-const second=document.querySelector(".second >p");
 const underBtn=document.querySelector(".footer > a");
 // querySelectorAll로 불러오는 요소는 요소를 하나로 불러오는게 아니라
 // 각각의 요소들을 배열 형태로 불러오게 됩니다.
@@ -19,7 +18,7 @@ window.addEventListener("scroll", function(){
     scrollYpos = window.scrollY;
     console.log(scrollYpos);
 
-    if(scrollYpos >10 && scrollYpos <= 400){
+    if(scrollYpos >0 && scrollYpos <= 2120){
         body.style.backgroundColor="red";
          slideEl[0].classList.add("on") ;
          slideEl[1].classList.add("on") ;
@@ -27,26 +26,25 @@ window.addEventListener("scroll", function(){
          slideElR[1].classList.add("on");
          slide.style.display="block";
          underBtn.style.color="white";       
-        slideP.style.transform = `scale(${20/scrollYpos})`;
-        console.log(400/scrollYpos);
+         slideP.style.transform = `scale(${70/scrollYpos})`;
+        console.log(scrollYpos);
      
     }
-    else if(scrollYpos >400 && scrollYpos <=800){
-        slideEl[0].classList.remove("on") ;
-        slideEl[1].classList.remove("on") ;
-        slideElR[0].classList.remove("on");
-        slideElR[1].classList.remove("on");
-        // slideEl[0].style.display="none";
-        // slideEl[1].style.display="none";
-        // slideElR[0].style.display="none";
-        // slideElR[1].style.display="none";
-        // slideP.style.display="none";
-        body.style.backgroundColor="aqua";
-        slideP.style.transform = `scale(${20/scrollYpos})`;
-   
-        
+  //가운데 글자가 없어지는 시점
+    else if(scrollYpos >2120 && scrollYpos <=2400){
+        body.style.backgroundColor="yellow";      
     }
+
+    
+ // 슬라이드 문자가 흐릿해지는 시점
+    else if(scrollYpos >2401 && scrollYpos <10000){
+        slide.style.opacity= `${1600/scrollYpos}`;
+        let opacity = slide.style.opacity;
+        console.log(opacity);
+    }
+
     else{
+     
         body.style.backgroundColor="aqua";
         underBtn.style.color="rgb(209, 209, 117)";
             
