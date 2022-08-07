db4fc65988cce2197887136a40c10edab78526b1
BogusD <qazxwd44@hanmail.net>
2022-08-08 01:38:40 +0900

    update mainpage

--- mainpage/js/main.js
+++ mainpage/js/main.js
@@ -1,10 +1,15 @@
-const slide = document.querySelector(".slide_text");
-const body = document.querySelector("body");
+
+const bodyEl = document.querySelector("body");
+
+
+const slidetext = document.querySelector(".slide_text");
+const slideEl2 =document.querySelector(".slide_text_2")
 const slideEl = document.querySelectorAll(".slide")
 const slideElR = document.querySelectorAll(".slide_R")
-const slideEl2 =document.querySelector(".slide_text_2")
-const slideP=document.querySelector(".slogan")
+const slogan =document.querySelector(".slogan")
 const underBtn=document.querySelector(".footer > a");
+const staggerItem=document.querySelectorAll(".stagger_item");
+
 // querySelectorAll로 불러오는 요소는 요소를 하나로 불러오는게 아니라
 // 각각의 요소들을 배열 형태로 불러오게 됩니다.
 // 따라서 각각의 요소에 접근해서 on 클래스를 부여하셔야 합니다!
@@ -17,49 +22,94 @@ let scrollYpos;
 
 window.addEventListener("scroll", function(){
     scrollYpos = window.scrollY;
-    console.log(scrollYpos);
 
-    if(scrollYpos >0 && scrollYpos <= 4600){
-        body.style.backgroundColor="red";
-         slideEl[0].classList.add("on") ;
-         slideEl[1].classList.add("on") ;
-         slideElR[0].classList.add("on");
-         slideElR[1].classList.add("on");
+    if(scrollYpos > 0 && scrollYpos <= 4600){
+         bodyEl.style.backgroundColor="red";
          underBtn.style.color="white";       
-         slideP.style.transform = `scale(${80/scrollYpos})`;
-         console.log(scrollYpos);
-     
+         slogan.style.transform = `scale(${80/scrollYpos})`;
+         console.log(scrollYpos);    
     }
-    
-  //가운데 글자가 없어지는 시점
-    else if(scrollYpos >4601 && scrollYpos <=5600){
-        body.style.backgroundColor="yellow";   
+    // 슬라이드가 시작되는 시점
+     if(scrollYpos > 0 && scrollYpos <13000){
+        slideEl[0].classList.add("on") ;
+        slideEl[1].classList.add("on") ;
+        slideElR[0].classList.add("on");
+        slideElR[1].classList.add("on");
+        
+    }
+
+  //가운데 문자가 없어지는 시점
+    if(scrollYpos > 4601 && scrollYpos <=5600){
+        bodyEl.style.backgroundColor="yellowgreen";   
+        slogan.style.display="none";
+        // alert("!");
  
     }
-
     
  //슬라이드 문자가 흐릿해지는 시점
-    else if(scrollYpos >5600 && scrollYpos <20000){
-        slide.style.opacity= `${3500/scrollYpos}`;
-        let opacity = slide.style.opacity;
-        console.log(opacity);
+    if(scrollYpos >5600 && scrollYpos <10000){
+        slidetext.style.opacity= `${3500/scrollYpos}`;
+        console.log(scrollYpos);  
     }
-  //  두번째 컨텐츠 요소가 나타나는 시점
-    // if(scrollYpos <15000){
-    //     slideEl[0].classList.add("on") ;
-    //     slideEl[1].classList.add("on") ;
-    //     slideElR[0].classList.add("on");
-    //     slideElR[1].classList.add("on");
-    // }
-    // else if(scrollYpos>=15000  ){
-    //     slideEl2.style.display= "block";
-    //     slideEl2.style.opacity = `${scrollYpos/3500}`      
-       
+    // slide_text_2 텍스트의 opacity가 조금씩 1로 바뀌는 시점
+    // if(scrollYpos >5600 && scrollYpos <???){
     // }
 
-    else{
-        body.style.backgroundColor="aqua";
-        underBtn.style.color="rgb(209, 209, 117)";
-            
+    // <opacity + 글자 위치 변경>
+    
+    function phase2(){
+
     }
+
+    let staggerItemlen = staggerItem.length;
+    let dx = 50;
+    const 시작 = 8000;
+    const 종료 = 시작 + dx*10;
+    for(let i=0; i<staggerItemlen; i++){
+        if(scrollYpos >= 시작 + i*dx && scrollYpos < 종료 + i*dx ){
+            bodyEl.style.backgroundColor="skyblue";
+            console.log(scrollYpos);  
+            staggerItem[i].style.transform= `translateY(${(시작+ i*dx - scrollYpos)/4 }px)`;
+            a = 시작 + i*dx
+            b = 종료 + i*dx
+            console.log('a,b,i',a,b,i)
+            staggerItem[i].style.opacity = `${(scrollYpos-a / b-a) /100}`
+
+            let opacity= staggerItem[0].style.opacity;
+            console.log('opacity',opacity);
+
+        } 
+    }
+
+    // <opacity 공식>
+    // dx = 50 일때
+    // a : 8000 + i*dx
+    // b : 8400 + i*dx
+    //                 a      b
+    // i : 0  => scr : 8000 ~ 8400 | i*dx = 50
+    // i : 1  => scr : 8050 ~ 8450 | i*dx = 100
+    // ...
+    // i : 10 => scr : 8500 ~ 8900 | i*dx = 500
+    //                 0    -    1
+    // target : 0/400 ~ 400/400 (scrollYpos-a / b-a)
+
+
+    // <화이트 - 블랙>
+    // 첫번째 슬라이드가 사라지는 지점
+    if(scrollYpos>=13000) {
+        slidetext.style.display="none";
+    }
+
+    // < intriguing - surprising>
+
+    //세번째 슬라이드 시작지점
+
+
 })
+
+
+    // function funk(){
+    //     slideEl2.style.display= "none";
+    //     bodyEl.style.backgroundColor="aqua";
+    //     underBtn.style.color="rgb(209, 209, 117)";
+    //     slogan.style.display="block"; 
