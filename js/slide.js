d73dab271ad50e30f95d53bc835b02c187236c61
wan <2jin_j@naver.com>
2022-08-10 17:00:24 +0900

    시작 테스트

--- js/slide.js
+++ js/slide.js
@@ -6,22 +6,21 @@ const deg = 360 / len;
 
 let korea = document.querySelector(".korea");
 
+// drag rotate
+let startX, endX;
+let temp = 0;
+body.addEventListener("mousedown", function (e) {
+  startX = e.screenX;
 
-//drag rotate
-// let startX, endX;
-// let temp = 0;
-// body.addEventListener("mousedown", function (e) {
-//   startX = e.screenX;
+  figure.style.cursor = "grabbing";
+});
+body.addEventListener("mouseup", function (e) {
+  endX = e.screenX;
+  frame.style.transform = `rotate(${(temp = temp - (startX - endX) / 15)}deg)`;
+  console.log(temp);
 
-//   figure.style.cursor = "grabbing";
-// });
-// body.addEventListener("mouseup", function (e) {
-//   endX = e.screenX;
-//   frame.style.transform = `rotate(${(temp = temp - (startX - endX) / 15)}deg)`;
-//   console.log(temp);
-
-//   figure.style.cursor = "grab";
-// });
+  figure.style.cursor = "grab";
+});
 
 for (let i = 0; i < len; i++) {
   list[i].style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
@@ -87,7 +86,7 @@ figure.addEventListener("click", (e) => {
           prev = i - 1;
           next = i + 1;
         }
-        
+
         list[prev].style.opacity = "0";
         list[next].style.opacity = "0";
         header.style.opacity = "0";
@@ -105,51 +104,56 @@ figure.addEventListener("click", (e) => {
   }
 });
 
-
-const colorList = ["rgb(157, 138, 130)", "rgb(198, 145, 167)", "rgb(222, 172, 64)","rgb(61, 120, 81)" , "rgb(60, 107, 150)", "rgb(121, 90, 132)","rgb(190, 103, 90)","rgb(148, 148, 148)"];
+const colorList = [
+  "rgb(157, 138, 130)",
+  "rgb(198, 145, 167)",
+  "rgb(222, 172, 64)",
+  "rgb(61, 120, 81)",
+  "rgb(60, 107, 150)",
+  "rgb(121, 90, 132)",
+  "rgb(190, 103, 90)",
+  "rgb(148, 148, 148)",
+];
 const frontPage = document.querySelectorAll(".page_front");
 const photoCard = document.querySelectorAll(".photo_card");
 
-for(let i=0; i<len; i++){
+for (let i = 0; i < len; i++) {
   frontPage[i].style.backgroundColor = colorList[i];
   photoCard[i].style.backgroundColor = colorList[i];
 }
 
+// // drag move
+// let pressed = false;
+// let startx, finishx;
+// let dragSum = 0;
+// // let vh = document.body.clientHeight;
 
+// figure.addEventListener("mousedown", e => {
+//     pressed = true;
+//     startx = e.offsetX;
+//     figure.style.cursor = "grabbing";
+// })
 
-// drag move 
-let pressed = false;
-let startx, finishx;
-let dragSum = 0;
-// let vh = document.body.clientHeight;
+// figure.addEventListener("mouseenter", () => {
+//     figure.style.cursor = "grab";
+// })
 
-figure.addEventListener("mousedown", e => {
-    pressed = true;
-    startx = e.offsetX;
-    figure.style.cursor = "grabbing";
-})
+// figure.addEventListener("mouseup", e => {
+//     figure.style.cursor = "grab";
+// })
 
-figure.addEventListener("mouseenter", () => {
-    figure.style.cursor = "grab";
-})
+// window.addEventListener("mouseup", () => {
+//     pressed = false;
 
-figure.addEventListener("mouseup", e => {
-    figure.style.cursor = "grab";
-})
+// })
 
-window.addEventListener("mouseup", () => {
-    pressed = false;
+// //마우스 드래그 시
+// figure.addEventListener("mousemove", e => {
+//     if (!pressed) return
+//     e.preventDefault();
+//     finishx = e.offsetX;
+//     dragSum -= startx - finishx;
+//     console.log(dragSum/20);
 
-})
-
-//마우스 드래그 시
-figure.addEventListener("mousemove", e => {
-    if (!pressed) return
-    e.preventDefault();
-    finishx = e.offsetX;
-    dragSum -= startx - finishx; 
-    console.log(dragSum/20);
-
-    frame.style.transform = `rotate(${dragSum/150}deg)`;    
-})
-
+//     frame.style.transform = `rotate(${dragSum/150}deg)`;
+// })
